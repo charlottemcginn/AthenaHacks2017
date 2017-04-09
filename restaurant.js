777761bcb37627ba676f0d27e9882ecd3f80ced3
EEscalas <elena@agonswim.com>
2017-04-09 10:05:09 -0700

    Beaut
    almost done

--- restaurant.js
+++ restaurant.js
@@ -9,6 +9,27 @@ var subtract = (function () {
     return function () {return counter -= 1;}
 })();
 
+if (typeof(Storage) !== "undefined") {
+var counter1 = 0;
+var add1 = (function () {
+    
+    return function () {return counter1 += 1;}
+})();
+var subtract1 = (function () {
+    
+    return function () {return counter1 -= 1;}
+})();
+
+if (typeof(Storage) !== "undefined") {
+var counter2 = 0;
+var add2 = (function () {
+    
+    return function () {return counter2 += 1;}
+})();
+var subtract2 = (function () {
+    
+    return function () {return counter2 -= 1;}
+})();
 
 function ADD(){
     document.getElementById("demo").innerHTML = add();
@@ -20,7 +41,22 @@ function SUBTRACT(){
     // Sorry! No Web Storage support..
 }
 
+function ADD1(){
+    document.getElementById("demo1").innerHTML = add1();
+}
+function SUBTRACT1(){
+    document.getElementById("demo1").innerHTML = subtract1();
+}
+} else {
+    // Sorry! No Web Storage support..
+}
 
-
-
-
+function ADD2(){
+    document.getElementById("demo2").innerHTML = add2();
+}
+function SUBTRACT2(){
+    document.getElementById("demo2").innerHTML = subtract2();
+}
+} else {
+    // Sorry! No Web Storage support..
+}
